38149ae83e0100427de8bbe3474da043b527ff34
ai-zaytsev <ai.zaytsev@yandex.ru>
2022-05-13 18:39:09 +0300

    le grande changes

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# yamdb_final
+# My API project
+## Project is aimed to accumulate users' reviews on different subjects
+### Stack used: 
+- Python
+- Django
+
+### Setup instructions:
+1. Install docker: https://docs.docker.com/engine/install/ubuntu/
+
+2. Clone 
+```
+docker pull aizaytsev/api_yamdb
+```
+
+3. Configure .git:
+```
+DB_ENGINE=django.db.backends.postgresql
+DB_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=db
+DB_PORT=5432
+SECRET_KEY=*YOUR_SECRET_KEY*
+```# yamdb_final# yamdb_final
 yamdb_final
+yamdb_final
+
+4. Build
+```
+docker-compose up -d --build
+```
+
+5. Migrate, create superiser, collect static
+```
+docker-compose exec web python manage.py migrate
+docker-compose exec web python manage.py createsuperuser
+docker-compose exec web python manage.py collectstatic --no-input
+```
+
+6. Go to http://localhost/
+
+
+*Authors*: Alex, Alexander, Ilya 
+
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 
 DEBUG = False
 
@@ -55,12 +55,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
