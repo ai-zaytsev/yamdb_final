57d02685749d11fa098a4653a1a6f34129ba658a
ai-zaytsev <ai.zaytsev@yandex.ru>
2022-05-19 17:19:29 +0300

    allowed_hosts >> .env, flow fixed

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
